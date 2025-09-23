e25d39b624a0442a4a6d8090a05b19c70b3e38f5
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-09-22 23:00:49 -0300

    Turn QuizAPI into an Abstract class
    This makes possible to implement the factory method pattern, since I can't do this with interfaces due to the `static` keyword.

--- src/quiz/rules/quiz.ts
+++ src/quiz/rules/quiz.ts
@@ -1,9 +1,21 @@
 import { Quiz } from "../models/quiz"
+import { QuizSettings } from "../models/settings"
 
 export interface IntoQuiz {
     intoQuiz(): Promise<Quiz>
 }
 
-export interface QuizApi extends IntoQuiz {
-    url: string
+export abstract class QuizApi implements IntoQuiz {
+    readonly url: string
+
+    protected constructor(url: string) {
+        this.url = url
+    }
+
+    abstract intoQuiz(): Promise<Quiz>
+
+    // Subclasses should override this static factory method.
+    static from(settings: QuizSettings): QuizApi {
+        throw new Error('QuizApi.from() must be implemented by subclasses')
+    }
 }
